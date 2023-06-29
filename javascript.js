38d5dcf4a6d3362290f9facb136b14a143c6bf07
kenyounot123 <kenlu519@gmail.com>
2023-06-29 10:24:24 -0400

    Update createSketchboard

--- javascript.js
+++ javascript.js
@@ -1,30 +1,25 @@
 
-let gridSize = 0;
+const gridSize = 600;
 
 function createSketchboard(size) {
-  gridSize = size * size;
-  const createPlayingField = document.createElement('div')
-  createPlayingField.setAttribute('id', 'playingGrid')
-  createPlayingField.style.height = `${gridSize}px`;
-  createPlayingField.style.width = `${gridSize}px`;
-  document.body.appendChild(createPlayingField);
-  makeRows();
-  return createPlayingField;
+  let rowByColumn = gridSize / size;
+  makeRows(rowByColumn);
+  return; 
 }
 
 
 
-function makeRows() {
-  let rowByColumn = Math.sqrt(gridSize);  
+function makeRows(numberOfGrids) {
+  gridDimension = numberOfGrids - 2; 
   const playingField = document.getElementById('playingGrid');
-  for (let i = 0; i < rowByColumn; i++){
+  for (let i = 0; i < (gridSize / numberOfGrids); i++){
     const rowContainer = document.createElement('div');
     rowContainer.classList.add('rowContainer');
-    for (j = 0; j < rowByColumn; j++) {
+    for (j = 0; j < (gridSize / numberOfGrids); j++) {
       const rowGrids = document.createElement('div');
       rowGrids.classList.toggle('grid');
-      rowGrids.style.height = `${rowByColumn - 2}px`;
-      rowGrids.style.width = `${rowByColumn - 2}px`;
+      rowGrids.style.height = `${gridDimension}px`;
+      rowGrids.style.width = `${gridDimension}px`;
       rowContainer.appendChild(rowGrids);
     }
     playingField.appendChild(rowContainer);
@@ -36,10 +31,18 @@ Create 16x16 grid
 think of a div or container as a single grid or square.
 
 To make a row  {
+  get playing field size and divide by rowByColumn to adjust size of grids
   create a div as a parent container 
   create a div and give it the class 'grid'
   append to parent container
   display parent container as flex
 }
 
+*/
+/*
+const createPlayingField = document.createElement('div')
+createPlayingField.setAttribute('id', 'playingGrid')
+createPlayingField.style.height = `${gridSize}px`;
+createPlayingField.style.width = `${gridSize}px`;
+document.body.appendChild(createPlayingField);
 */
